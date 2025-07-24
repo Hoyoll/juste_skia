787ffd7db564919940863670c6e795e17e1098b2
Effendi <effendi.saputra01@gmail.com>
2025-07-24 23:44:35 +0700

    some cleaning up

--- src/constants.rs
+++
@@ -1,2 +0,0 @@
-pub const LOAD_IMG_URL: i8 = -1;
-pub const LOAD_IMG_SYS: i8 = -2;
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 pub mod app;
-pub mod constants;
 pub mod io;
 pub mod passes;
 pub mod renderer;
-pub use constants::*;
+pub use app::*;
 pub use passes::*;
 pub use renderer::*;
